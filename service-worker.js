bb631526fb7dc29ded6f1705ef4a7802fc710851
Tejaswini Dhupad <tejdhuppad@gmail.com>
2025-11-12 12:59:45 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c3cfd80aeb3c5e532f2543a2732a03fb.js"
+  "/precache-manifest.1c6b5b7cf143d76d2c31d68a03a4c62c.js"
 );
 
 self.addEventListener('message', (event) => {
